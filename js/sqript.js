6553f3d8c6427fe0e74872a416f1bd3707789a01
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-08-19 13:36:53 +0300

    first commit

---
+++ js/sqript.js
@@ -0,0 +1,10 @@
+﻿var $inputs = $('.ftooltip .ftooltip__txt');
+
+$inputs.on('mousemove', function() {
+			  var $tips = $(this).siblings('.ftooltip__tips');
+			  $tips.show();
+})
+      .on('mouseleave', function() {
+			  var $tips = $(this).siblings('.ftooltip__tips');
+			  $tips.hide();
+});
